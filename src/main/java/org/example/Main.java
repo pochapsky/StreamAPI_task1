cd008cae460978ba86d6fa3c7b6fb060f9074e16
Konstantin Pochapsky <chernoknignikvladikus@yandex.ru>
2023-06-11 17:02:11 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        intList.sort(Comparator.naturalOrder());
+        for (int i : intList) {
+            if (i > 0 && i % 2 == 0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
